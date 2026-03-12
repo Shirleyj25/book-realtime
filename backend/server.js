85f175f145702c65a14aa897578369dd6166285b
SHIRLEY J <johnshirley29229@gmail.com>
2026-03-12 16:34:43 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let books = [];
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.emit("books", books);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+
+app.post("/books", (req, res) => {
+  const book = {
+    id: Date.now(),
+    title: req.body.title,
+    author: req.body.author,
+  };
+
+  books.push(book);
+
+  io.emit("newBook", book);
+
+  res.json(book);
+});
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+  console.log("Server running on port 5000");
+});
